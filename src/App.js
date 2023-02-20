6f24c2d008045ed1053334b378ebda2df0ad059f
TQT67 <trangtrinh000000@gmail.com>
2023-02-20 14:10:51 +0700

    th2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ProtectedGroup from "./ProtectedGroup";
+import PrivateGroup from "./PrivateGroup";
+import { createBrowserRouter, Route, RouterProvider } from "react-router-dom";
 
-function App() {
+const router = createBrowserRouter([
+  {
+    path: "/private-group",
+    element: (
+      <ProtectedGroup isMember={false}>
+        <Route path="/private-group" component={PrivateGroup} />
+      </ProtectedGroup>
+    ),
+  },
+]);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router}>
+      <Route exact path="/private-group">
+        <PrivateGroup />
+      </Route>
+    </RouterProvider>
   );
-}
+};
 
 export default App;
